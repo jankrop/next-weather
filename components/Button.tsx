639f935a803bed0f43946b20fae34bb973d25120
Jan Kropiwnicki <jan.kropiwnicki@infotech.edu.pl>
2025-10-17 10:40:04 +0200

    What a gorgeous button

---
+++ components/Button.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from "react";
+
+export default function Button({
+    children
+}: {
+    children: ReactNode
+}) {
+    return (
+        <button className="
+            rounded-lg relative group bg-gray-800 cursor-pointers active:scale-95 transition-transform duration-200
+        ">
+            <div className="
+                absolute -z-1 rounded-lg bg-gradient-to-r from-teal-400 via-blue-500 to-purple-500 w-full h-full
+                blur-md opacity-0 group-hover:opacity-50 transition-opacity duration-200
+            "/>
+            <div className="
+                absolute z-0 rounded-lg bg-gradient-to-r from-teal-400 via-blue-500 to-purple-500 w-full h-full
+                opacity-0 group-hover:opacity-100 transition-opacity duration-200
+            " />
+            <div className="relative rounded-lg z-1 bg-gray-950 px-3 py-1.5 text-lg text-white font-bold m-[1px]">
+                {children}
+            </div>
+        </button>
+    )
+}
